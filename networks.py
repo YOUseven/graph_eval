bd95e6a2ee75424e88440134dbc5cd7fd8c940ec
YOUseven <ysq96@126.com>
2021-09-09 15:31:29 +0800

    fix the bug of network.py

--- networks.py
+++ networks.py
@@ -19,22 +19,15 @@ def network_scale(G, vis=True):
     dens_G = nx.density(G)
 
     all_nodes_degree = dict(G.degree())
-    avg_deg = round(sum(all_nodes_degree.values())/len(all_nodes_degree))
+    avg_deg = round(sum(all_nodes_degree.values())/len(all_nodes_degree), 4)
 
     iso_nodes = [item for item in nx.isolates(G)]
     cnt_iso_nodes = len(iso_nodes)
 
     if vis:
-        print("the number of edges: {}"+\
-              "the number of nodes: {}"+\
-              "the density of G: {}"+\
-              "the average of degrees: {:.4f}"+\
-              "the number of isolated nodes: {}".format(cnt_edges, cnt_nodes,
-                                                        dens_G, avg_deg,
-                                                        cnt_iso_nodes))
-    res = cnt_edges, cnt_nodes, \
-          dens_G, avg_deg, \
-          cnt_iso_nodes
+        print("the number of edges: {}\nthe number of nodes: {}\nthe density of G: {}\nthe average of degrees: {:.4f}\nthe number of isolated nodes: {}\n"
+              .format(cnt_edges, cnt_nodes, dens_G, avg_deg, cnt_iso_nodes))
+    res = cnt_edges, cnt_nodes, dens_G, avg_deg, cnt_iso_nodes
     return res
 
 def count_nodes_degree(G, vis=True):
@@ -48,7 +41,7 @@ def count_nodes_degree(G, vis=True):
     """
     degree_0, degree_1, degree_big = [], [], []
     for node in G.nodes:
-        d = graph.degree(node)
+        d = G.degree(node)
         if d == 0:
             degree_0.append(node)
         elif d == 1:
@@ -57,9 +50,8 @@ def count_nodes_degree(G, vis=True):
             degree_big.append(node)
 
     if vis:
-        print("the number of isolated nodes: {}" + \
-              "the number of 1-degree nodes: {}" + \
-              "the number of greater than 1-degree nodes: {}".format(degree_0, degree_1, degree_big))
+        print('the number of isolated nodes: {}\nthe number of 1-degree nodes: {}\nthe number of greater than 1-degree nodes: {}'
+              .format(len(degree_0), len(degree_1), len(degree_big)))
 
     res = degree_0, degree_1, degree_big
     return res
